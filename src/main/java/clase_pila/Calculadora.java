a9fc11b7f9f1958e1e273e612ad2c7cc5ef67e95
unknown <lj924618@gmail.com>
2024-12-05 11:24:54 -0500

    hola

---
+++ src/main/java/clase_pila/Calculadora.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase_pila;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author SO-LAB1-PC12
+ */
+public class Calculadora {
+ 
+
+    public Calculadora() {
+        
+    }
+
+    
+    
+
+ public void rehacer(Integer suma){
+        pilarehacer.push(contenido);
+        contenido += suma;
+        pilaresultado.clear();
+        System.out.println("Contenido actual: " + contenido);
+    }
+
+  public void suma(){
+  
+  }
+    
+}
+
+
+
+
+    public EditorTexto() {
+        this.pilaDeshacer = new Stack<>();
+        this.pilaRehacer = new Stack<>();
+        this.contenido = "";
+    }
+    
+    public void escribir(String texto){
+        pilaDeshacer.push(contenido);
+        contenido += texto;
+        pilaRehacer.clear();
+        System.out.println("Contenido actual: " + contenido);
+    }
+    
+    public void deshacer(){
+        if (!pilaDeshacer.isEmpty()){
+            pilaRehacer.push(contenido);
+            contenido = pilaDeshacer.pop();
+            System.out.println("Deshacer: " + contenido);
+        } else {
+            System.out.println("No hay acciones para deshacer");
+        }
+    }
+    
+    public void rehacer(){
+        if (!pilaRehacer.isEmpty()){
+            pilaDeshacer.push(contenido);
+            contenido = pilaRehacer.pop();
+            System.out.println("Rehacer: " + contenido);
+        } else {
+            System.out.println("No hay acciones para rehacer");
+        }
+    }
---
+++ src/main/java/com/mycompany/leoneljimenez/LeonelJimenez.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.leoneljimenez;
+
+/**
+ *
+ * @author SO-LAB1-PC12
+ */
+public class LeonelJimenez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
